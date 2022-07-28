244e08100f2b676c589759cfcc7ea3880c7bfd2a
luke.park <luke.park@cupix.com>
2022-07-28 19:22:11 +0900

    Upload

---
+++ android/app/src/main/kotlin/com/example/plugin_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.plugin_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/person.dart
@@ -0,0 +1,13 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class Person {
+  /// The generated code assumes these values exist in JSON.
+  final String firstName, lastName;
+
+  /// The generated code below handles if the corresponding JSON value doesn't
+  /// exist or is empty.
+  final DateTime? dateOfBirth;
+
+  Person({required this.firstName, required this.lastName, this.dateOfBirth});
+}
